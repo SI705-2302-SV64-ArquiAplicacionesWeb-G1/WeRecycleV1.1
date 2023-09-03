9b70809e5d9a04b3abcc179b12600d1eb6fdd8e6
Gabriel Meza Prada <standbyg2@gmail.com>
2023-09-03 17:58:09 -0500

    Fixing errors

---
+++ src/main/java/pe/edu/upc/aww/werecycle/controllers/UserController.java
@@ -0,0 +1,31 @@
+package pe.edu.upc.aww.werecycle.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.aww.werecycle.dtos.UserDTO;
+import pe.edu.upc.aww.werecycle.entities.Useror;
+import pe.edu.upc.aww.werecycle.serviceinterfaces.IUserService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private IUserService uS;
+    @PostMapping
+    public void registrar(@RequestBody UserDTO dto){
+        ModelMapper m = new ModelMapper();
+        Useror u =m.map(dto, Useror.class);
+        uS.insert(u);
+    }
+    @GetMapping
+    public List<UserDTO>listar(){
+        return uS.list().stream().map(x->{
+            ModelMapper m = new ModelMapper();
+            return m.map(x,UserDTO.class);
+        }).collect(Collectors.toList());
+    }
+}
